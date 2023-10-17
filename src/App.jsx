8564dd96e7b85b9fee059386c9e60fc34e2c8ef1
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-10-17 12:40:06 +0530

    status using ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-
-
 function App() {
-const status=false;
-switch(status){
-case true:return<button>LogOut</button>
-case false:return <button>LogIn</button>
-} 
-    }
-export default App
- 
+  const status = false;
+  return(
+  <div>{status ? <button>LogOut</button> : <button>LogIn</button>}</div>
+  )
+}
+export default App;
